c4ab50c8b1f3148eb7ae0aa8493e99afc291c477
JaanJahimees <jaanjahilo@outlook.com>
2019-03-05 08:51:03 +0200

    Getting Device Info done.

---
+++ HWMonitor/HWMonitor/GetHardwareInfo.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.Hardware;
+using Xamarin.Essentials;
+using Android.Util;
+
+namespace HWMonitor
+{
+    public class GetHardwareInfo
+    {
+        public string DeviceModel { get; set; }
+        public string DeviceManufacturer { get; set; }
+        public string DeviceName { get; set; }
+        public string DeviceVersion { get; set; }
+        public DevicePlatform DevicePlatform { get; set; }
+        public DeviceIdiom DeviceIdiom { get; set; }
+        public DeviceType DeviceType { get; set; }
+
+        public void GetDeviceInfo()
+        {
+            // Device Model
+            DeviceModel = DeviceInfo.Model;
+
+            // Manufacturer
+            DeviceManufacturer = DeviceInfo.Manufacturer;
+
+            // Device Name
+            DeviceName = DeviceInfo.Name;
+
+            // Operating System Version Number
+            DeviceVersion = DeviceInfo.VersionString;
+
+            // Platform
+            DevicePlatform = DeviceInfo.Platform;
+
+            // Idiom
+            DeviceIdiom = DeviceInfo.Idiom;
+
+            // Device Type
+            DeviceType = DeviceInfo.DeviceType;
+        }
+    }
+}
--- HWMonitor/HWMonitor/MainActivity.cs
+++ HWMonitor/HWMonitor/MainActivity.cs
@@ -6,6 +6,7 @@ using Android.Runtime;
 using Android.Widget;
 using Android.Views;
 using Toolbar = Android.Support.V7.Widget.Toolbar;
+using Xamarin.Essentials;
 
 namespace HWMonitor
 {
@@ -22,6 +23,15 @@ namespace HWMonitor
             SetSupportActionBar(toolbar);
             SupportActionBar.Title = "Hardware monitor";
             //ActionBar.SetHomeAsUpIndicator(Resource.Drawable.settingsBtn);
+
+            //Initialize Xamarin Essentials
+            Platform.Init(this, savedInstanceState);
+
+            //For debug
+            var hwInfo = new GetHardwareInfo();
+            hwInfo.GetDeviceInfo();
+            var text = hwInfo.DeviceModel;
+            var a = 0;
         }
 
         public override bool OnCreateOptionsMenu(IMenu menu)
@@ -35,6 +45,14 @@ namespace HWMonitor
             Toast.MakeText(this, "Action selected: " + item.TitleFormatted,
                 ToastLength.Short).Show();
             return base.OnOptionsItemSelected(item);
+        }
+
+        //Ask for permission to allow Xamarin Essentials
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
 }
